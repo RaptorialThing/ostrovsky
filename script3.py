5bc01466fc9cc41c3e36a7fc6379e7aab0e5d848
RaptorialThing <ruzal.gabdrahmanoff@yandex.ru>
2022-05-11 04:42:11 +0300

    add new script - test csvsql

--- script3.py
+++ script3.py
@@ -1,6 +1,17 @@
 # -*- coding: cp1251 -*-
 from csvkit.utilities.csvsql import CSVSQL
 
-args_query = ['--query','select APPLICATION_DT as Date, INTERNAL_ORG_ORIGINAL_RK as Pos,LOAN_AMOUNT as \'Сумма выдач\'  from data','data.csv']
+s = u'Сумма выдач'
+column_name_summa = s.encode('cp1251')
+column_name_summa = column_name_summa.decode('utf8')
+args_query = ['--query',"""
+
+select  DISTINCT  data.INTERNAL_ORG_ORIGINAL_RK, data.APPLICATION_DT, sum(pos_tb.LOAN_AMOUNT)
+from data  
+left join (select APPLICATION_DT, LOAN_AMOUNT, INTERNAL_ORG_ORIGINAL_RK, ACCOUNT_RK from data) as  pos_tb on (data.ACCOUNT_RK = pos_tb.ACCOUNT_RK)  
+group by  data.INTERNAL_ORG_ORIGINAL_RK,data.APPLICATION_DT
+order by  data.INTERNAL_ORG_ORIGINAL_RK,data.APPLICATION_DT
+
+""" ,'data.csv']
 result  = CSVSQL(args_query)
 print(result.main())
