6c87111052fc7afcf83ef94110f38b4cc29e89c2
RaptorialThing <ruzal.gabdrahmanoff@yandex.ru>
2022-05-16 02:24:15 +0300

    per date, per pos with dup pos

--- script3v2.py
+++ script3v2.py
@@ -93,33 +93,18 @@ order by data2.APPLICATION_DT,data2.INTERNAL_ORG_ORIGINAL_RK, data2.INTERNAL_ORG
 ;
 
 
-select data1.APPLICATION_DT, data5.APPLICATION_DT as Date, CAST(data5.INTERNAL_ORG_ORIGINAL_RK as INT) as Pos,
-case  when data1.APPLICATION_DT != data5.APPLICATION_DT then '0' else data5.summa
-end
+
+select data1.APPLICATION_DT as Date, CAST(data5.INTERNAL_ORG_ORIGINAL_RK as INT) as Pos,
+case  when data1.APPLICATION_DT != data5.APPLICATION_DT then 0 else data5.summa
+end as '%s'
 from (select * from t1) as data1
 inner join  (select  t6.LOAN_AMOUNT as summa, t6.* from t6) as data5
-on (data5.ACCOUNT_RK = data1.ACCOUNT_RK) 
+on (data5.ACCOUNT_RK = data1.ACCOUNT_RK)
 group by  data1.APPLICATION_DT,data5.APPLICATION_DT, data5.INTERNAL_ORG_ORIGINAL_RK
 order by  data1.APPLICATION_DT,data5.APPLICATION_DT, data5.INTERNAL_ORG_ORIGINAL_RK
+;
 
-
-""",'data.csv']
+""" % column_name_summa,'data.csv']
 result  = CSVSQL(args_query)
 
 print(result.main())
-
-#select 0, APPLICATION_DT as Date, CAST(INTERNAL_ORG_ORIGINAL_RK as INT) as Pos, sum(LOAN_AMOUNT) as '%s'  from t2 group by APPLICATION_DT,INTERNAL_ORG_ORIGINAL_RK  order by APPLICATION_DT  
-# select  data1.APPLICATION_DT, data1.INTERNAL_ORG_ORIGINAL_RK, data2.INTERNAL_ORG_ORIGINAL_RK, data2.LOAN_AMOUNT from t4 as data1
-# left join 
-# (select ACCOUNT_RK, APPLICATION_DT, INTERNAL_ORG_ORIGINAL_RK, LOAN_AMOUNT from t5) as data2
-# on (data1.INTERNAL_ORG_ORIGINAL_RK = data2.INTERNAL_ORG_ORIGINAL_RK)
-# order by data1.APPLICATION_DT, data1.INTERNAL_ORG_ORIGINAL_RK;
-
-# select data2.APPLICATION_DT as Date, CAST(data1.INTERNAL_ORG_ORIGINAL_RK as INT) as Pos,  data5.summa  as '%s'  from t3 as data1
-# left join (select APPLICATION_DT,INTERNAL_ORG_ORIGINAL_RK, t2.* from   t2 ) as data2
-# on data1.INTERNAL_ORG_ORIGINAL_RK = data1.INTERNAL_ORG_ORIGINAL_RK
-# inner join (select t5.APPLICATION_DT, t5.INTERNAL_ORG_ORIGINAL_RK, t5.LOAN_AMOUNT as summa, t5.* from t5 as t5) as data5
-# on (data5.INTERNAL_ORG_ORIGINAL_RK = data1.INTERNAL_ORG_ORIGINAL_RK)
-# group by data2.APPLICATION_DT, data1.INTERNAL_ORG_ORIGINAL_RK
-# order by data2.APPLICATION_DT,data2.INTERNAL_ORG_ORIGINAL_RK, data2.INTERNAL_ORG_ORIGINAL_RK
-# ;
